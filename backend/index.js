8f2ce2d922418df62186ab453b18e0886c16eab6
luceroz <53053902+luceroz@users.noreply.github.com>
2024-12-13 14:27:25 -0500

    testing simple database connectivity

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1dWZjZmtxdmRtenJzd3R2eW93Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQwNDg2ODIsImV4cCI6MjA0OTYyNDY4Mn0.bZbsj8N8_DE9PyeYNJz1S-2WvGoqImvDc7c6Qwu3rKI";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-app.get("/art", async (req, res) => {
+app.get("/", async (req, res) => {
   const { data, error } = await supabase.from("listings").select();
 
   console.log("Data Retrieved: ", data);
