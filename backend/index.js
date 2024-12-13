240482785448647cbf9144cedbba559542d03d3a
luceroz <53053902+luceroz@users.noreply.github.com>
2024-12-13 16:58:29 -0500

    Restoring database endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const { createClient } = require("@supabase/supabase-js");
+const { request } = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 const supabaseUrl = "https://buufcfkqvdmzrswtvyow.supabase.co";
 const supabaseKey =
@@ -12,10 +15,45 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 app.get("/", async (req, res) => {
   const { data, error } = await supabase.from("listings").select();
 
-  console.log("Data Retrieved: ", data);
-  console.log("Error: ", error);
+  if (error) {
+    console.log("Error: ", error);
+    res.send(error);
+  } else {
+    console.log("Successfully Retrieved");
+    res.send(data);
+  }
+});
 
-  res.send(data);
+app.post("/listing", async (req, res) => {
+  console.log("Attempting to add listing");
+  console.log("Request", req.body);
+
+  const title = req.body.title;
+  const medium = req.body.medium;
+  const describe = req.body.description;
+  const price = req.body.price;
+  const image = req.body.image;
+  const artist = req.body.artist_name;
+
+  const { data, error } = await supabase
+    .from("listings")
+    .insert({
+      title: title,
+      medium: medium,
+      description: describe,
+      price: price,
+      image: image,
+      artist_name: artist,
+    })
+    .select();
+
+  if (error) {
+    console.log("Error: ", error);
+    res.send(error);
+  } else {
+    console.log("Successfully Retrieved");
+    res.send(data);
+  }
 });
 
 app.listen(port, () => {
